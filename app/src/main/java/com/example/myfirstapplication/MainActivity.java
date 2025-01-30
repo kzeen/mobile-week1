7f36d60837ec7b538bf20e5537dabf68bb2bf87b
zeeny <157370421+kzeen@users.noreply.github.com>
2025-01-30 16:03:32 +0200

    Add button listener in java

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myfirstapplication;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Add java listener to button
+        Button bt = findViewById(R.id.button);
+//        We create an anonymous object/class from the interface to not have to create a class for each listener
+//        bt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//              Toast.makeText(MainActivity.this, "Submit with Java button", Toast.LENGTH_LONG).show());
+//            }
+//        });
+
+        //        There is also the option of creating a lambda expression - best way for event listening
+        bt.setOnClickListener(v -> {
+//            Fits multiple lines or one
+            Toast.makeText(this, "Submit with Java button", Toast.LENGTH_LONG).show();
+        });
     }
 
     public void save_user(View v) {
