acc666475c7e1019f2cec4aec4f6670ccd58b795
zeeny <157370421+kzeen@users.noreply.github.com>
2025-01-30 16:43:45 +0200

    Add new button

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.myfirstapplication;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,7 +29,30 @@ public class MainActivity extends AppCompatActivity {
         //        There is also the option of creating a lambda expression - best way for event listening
         bt.setOnClickListener(v -> {
 //            Fits multiple lines or one
-            Toast.makeText(this, "Submit with Java button", Toast.LENGTH_LONG).show();
+            EditText ed_firstname = findViewById(R.id.first_name);
+            String firstName = ed_firstname.getText().toString();
+
+            EditText ed_lastname = findViewById(R.id.last_name);
+            String lastName = ed_lastname.getText().toString();
+
+            if (TextUtils.isEmpty(firstName)) {
+                ed_firstname.setError("Fill your first name");
+                return;
+            }
+
+            if (TextUtils.isEmpty(lastName)) {
+                ed_lastname.setError("Fill your last name");
+                return;
+            }
+
+            String message = "Welcome " + firstName + " " + lastName;
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            ed_firstname.setText("");
+            ed_lastname.setText("");
+            ed_firstname.requestFocus();
+
+            TextView tv = findViewById(R.id.new_text);
+            tv.setText(message);
         });
     }
 
